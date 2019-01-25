14e8d4090365869de8510955ff172bc842954b3c
Michael S. Taylor (home) <mtaylor@semo.edu>
2019-01-24 19:19:31 -0600

    Reactive. Added lower plot of sampled freqs.

--- hwe_sampling/app.R
+++ hwe_sampling/app.R
@@ -8,6 +8,7 @@
 
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
 cols <-RColorBrewer::brewer.pal(3,"Dark2")
 names(cols) <- c("p2", "pq2", "q2")
@@ -25,25 +26,24 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-        helpText("Move the slider"),
         sliderInput("pop_size",
                     "Population Size",
                     min = 50,
                     max = 450,
                     value = 200,
-                    step = 50)
+                    step = 50),
+        sliderInput("p_freq",
+                    "Choose frequency of p:",
+                    min = 0,
+                    max = 1,
+                    value = 0.5,
+                    step = 0.05)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(align = "center",
          plotOutput("hwe_plot"),
-         sliderInput("p_freq",
-                     "Choose frequency of p:",
-                     min = 0,
-                     max = 1,
-                     value = 0.5,
-                     step = 0.01,
-                     width = "90%")
+         plotOutput("freq_plot")
       )
    )
 )
@@ -54,16 +54,14 @@ server <- function(input, output) {
   
 # Functions ---------------------------------------------------------------
 
-  cross <- function(parents)
-  {
+  cross <- function(parents) {
     offspring <- c('d', 'd') #initiate a child object
     offspring[1] <- sample(parents[1, ], 1)
     offspring[2] <- sample(parents[2, ], 1)
     return(offspring)
   }
   
-  random_mating <- function()
-  {
+  random_mating <- function() {
     tmp_pop <- pop
     for (n in 1:N)
     {
@@ -82,61 +80,69 @@ server <- function(input, output) {
 # Reactives ---------------------------------------------------------------
 
   sample_pop <- reactive({
-  })
-  
-   output$hwe_plot <- renderPlot({
-     
-     p <- 0.5
-     q <- 1 - p
-     N = input$pop_size
-     a_freq <- c(p, q)
-     
-     pop <- array(sample(genotypes, 2 * N, p = a_freq, replace = T), dim = c(N, 2))
-     
-     
-     I <- N
-     num_generations = 1
-     g_freq <- array(dim = c(N, 3))
-     p_vec <- array(dim = N)
-     for (i in 1:I)
-     {
-       p <- runif(1, 0, 1)
-       q <- 1 - p
-       a_freq <- c(p, q)
-       
-       pop[, 1] <- sample(genotypes, N, p = a_freq, replace = T)
-       pop[, 2] <- sample(genotypes, N, p = a_freq, replace = T)
-       
-       for (g in 1:num_generations)
-         random_mating()
-       
-       f_aa <- 0
-       f_Aa <- 0
-       f_AA <- 0
-       
-       for (n in 1:N)
-       {
-         if (identical(pop[n, ], c('A', 'A')))
-           f_AA = f_AA + 1
-         if (identical(pop[n, ], c('A', 'a')) ||
-             identical(pop[n, ], c('a', 'A')))
-           f_Aa = f_Aa + 1
-         if (identical(pop[n, ], c('a', 'a')))
-           f_aa = f_aa + 1
-         
-       }
-       f_aa <- f_aa / N
-       f_Aa <- f_Aa / N
-       f_AA <- f_AA / N
-       
-       g_freq[i, ] <- c(f_AA, f_Aa, f_aa)
-       p_vec[i] <- p
-     }
-     
-     hw <- data.frame(p = p_vec, 
-                p2 = g_freq[, 1],
-                pq2 = g_freq[, 2],
-                q2 = g_freq[, 3])
+      p <- 0.5
+      q <- 1 - p
+      N = input$pop_size
+      a_freq <- c(p, q)
+      
+      pop <- array(sample(genotypes, 2 * N, 
+                          p = a_freq, 
+                          replace = T), 
+                   dim = c(N, 2))
+      
+      
+      #     I <- N
+      num_generations = 1
+      g_freq <- array(dim = c(N, 3))
+      p_vec <- array(dim = N)
+      for (i in 1:N)
+      {
+        p <- runif(1, 0, 1)
+        q <- 1 - p
+        a_freq <- c(p, q)
+        
+        pop[, 1] <- sample(genotypes, N, p = a_freq, replace = T)
+        pop[, 2] <- sample(genotypes, N, p = a_freq, replace = T)
+        
+        for (g in 1:num_generations)
+          random_mating()
+        
+        f_aa <- 0
+        f_Aa <- 0
+        f_AA <- 0
+        
+        for (n in 1:N)
+        {
+          if (identical(pop[n, ], c('A', 'A')))
+            f_AA = f_AA + 1
+          if (identical(pop[n, ], c('A', 'a')) ||
+              identical(pop[n, ], c('a', 'A')))
+            f_Aa = f_Aa + 1
+          if (identical(pop[n, ], c('a', 'a')))
+            f_aa = f_aa + 1
+          
+        }
+        f_aa <- f_aa / N
+        f_Aa <- f_Aa / N
+        f_AA <- f_AA / N
+        
+        g_freq[i, ] <- c(f_AA, f_Aa, f_aa)
+        p_vec[i] <- p
+      }
+      
+      data.frame(p = p_vec, 
+                 p2 = g_freq[, 1],
+                 pq2 = g_freq[, 2],
+                 q2 = g_freq[, 3])
+    })
+
+
+# Plots -------------------------------------------------------------------
+
+
+  output$hwe_plot <- renderPlot(height = 300,{
+
+     hw <- sample_pop()
      
      p <-ggplot(data = hw) +
        xlim(c(0, 1)) +
@@ -161,6 +167,47 @@ server <- function(input, output) {
      
      print(p)
    })
+  
+  output$freq_plot <- renderPlot(height = 300, {
+    
+    freq <- input$p_freq
+    N <- input$pop_size
+    
+    
+    x <- rnorm(N, mean = freq, sd = 0.05)
+    hom1 <- function(x) x^2
+    hom2 <- function(x) (1 - x)^2
+    hets <- function(x) 2 * x * (1 - x)
+
+    genos <- c("p2", "2pq", "q2")
+    
+    df_sampled <- rnorm(N, freq, 0.07) %>% tibble(p2 = map_dbl(., hom1),
+                                     `2pq` = map_dbl(., hets),
+                                     q2 = map_dbl(., hom2)) %>% 
+      gather(key = genotype, value = freqs, contains("2")) %>% 
+      select(-`.`) %>% 
+      mutate(genotype = factor(genotype, levels = genos, ordered = TRUE))
+    
+    df <- data.frame(genotypes <- factor(genos, levels = genos, ordered = TRUE),
+                     freqs = c(freq^2, 2 * freq * (1-freq), (1 - freq)^2))
+
+    ggplot(df, aes(x = genotypes,
+                   y = freqs)) + ylim(0,1) +
+      geom_boxplot(data = df_sampled,
+                  aes(x = genotype,
+                      y = freqs,
+                      fill = genotype),
+                  alpha = 0.5) +
+      geom_point(aes(fill = genotypes),
+                 size = 3,
+                 shape = 21) +
+      scale_fill_brewer(type = "qual",
+                        palette = "Dark2",
+                        guide = FALSE) +
+      scale_color_brewer(type = "qual",
+                        palette = "Dark2",
+                        guide = FALSE)
+  })
 }
 
 # Run the application 
