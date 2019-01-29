3550f26966357eddc2b81b675036ea17e093f960
Mike Taylor <mtaylor@semo.edu>
2019-01-29 15:51:32 -0600

    Changed `values` to simple to avoid conflict converting to navBar.

--- hwe_practice/simple_hw.R
+++ hwe_practice/simple_hw.R
@@ -16,9 +16,9 @@ ui <- fluidPage(
       helpText("Press \"New problem\" for a new problem to solve. Press
                  \"Show answer\" to see the solution. Refer to your notes 
                  for details of the steps."),
-      actionButton("new_problem",
+      actionButton("new_simple_problem",
                    "New problem"),
-      actionButton("show_answer",
+      actionButton("show_simple_answer",
                    "Show answer"),
       uiOutput("equations")
     ),
@@ -47,31 +47,31 @@ server <- function(input, output) {
     withMathJax("$$p + q = 1$$ $$p^2 + 2pq + q^2 = 1$$")
   })
   
-  show_ans <- reactiveVal(FALSE)
+  show_simple_ans <- reactiveVal(FALSE)
   
-  values <- reactiveValues()
+  simple <- reactiveValues()
   
-  observeEvent(input$new_problem, {
-    show_ans(FALSE)
+  observeEvent(input$new_simple_problem, {
+    show_simple_ans(FALSE)
     
     LETTER <- sample(LETTERS, 1)
-    values$a1 <- paste0(LETTER, "<sub>1</sub>")
-    values$a2 <- paste0(LETTER, "<sub>2</sub>")
+    simple$a1 <- paste0(LETTER, "<sub>1</sub>")
+    simple$a2 <- paste0(LETTER, "<sub>2</sub>")
     
-    hom1 <- paste0(values$a1, values$a1)
-    het <- paste0(values$a1, values$a2)
-    hom2 <- paste0(values$a2, values$a2)
+    hom1 <- paste0(simple$a1, simple$a1)
+    het <- paste0(simple$a1, simple$a2)
+    hom2 <- paste0(simple$a2, simple$a2)
     
-    values$genotypes <- c(hom1, het, hom2)
-
-    values$allele1 <- round(runif(1, 0.1, 0.9), 3)
-    values$allele2 <- 1 - values$allele1
+    simple$genotypes <- c(hom1, het, hom2)
     
-    values$AA_freq <- round(values$allele1^2, 3)
-    values$Aa_freq <- round(2 * values$allele1 * values$allele2, 3)
-    values$aa_freq <- round(values$allele2^2, 3)
+    simple$allele1 <- round(runif(1, 0.1, 0.9), 3)
+    simple$allele2 <- 1 - simple$allele1
     
-   }, ignoreNULL = FALSE)
+    simple$AA_freq <- round(simple$allele1^2, 3)
+    simple$Aa_freq <- round(2 * simple$allele1 * simple$allele2, 3)
+    simple$aa_freq <- round(simple$allele2^2, 3)
+    
+  }, ignoreNULL = FALSE)
   
   output$question <- renderText({
     
@@ -80,63 +80,47 @@ server <- function(input, output) {
     
     if (allele_or_geno == 0) { # Start with allele freq
       if (one_or_two == 0){ # use p
-        sprintf("The frequency of allele %s is %0.3f.", values$a1, values$allele1)
+        sprintf("The frequency of allele %s is %0.3f.", simple$a1, simple$allele1)
       } else { #use q
-        sprintf("The frequency of allele %s is %0.3f.", values$a2, values$allele2)
+        sprintf("The frequency of allele %s is %0.3f.", simple$a2, simple$allele2)
       }
     } else { # Genotype freq
       if (one_or_two == 0){ # use p2
-        sprintf("The frequency of genotype %s is %0.3f.", values$genotypes[1], values$AA_freq)
+        sprintf("The frequency of genotype %s is %0.3f.", simple$genotypes[1], simple$AA_freq)
       } else {# use q2
-        sprintf("The frequency of genotype %s is %0.3f.", values$genotypes[3], values$aa_freq)
+        sprintf("The frequency of genotype %s is %0.3f.", simple$genotypes[3], simple$aa_freq)
       }
     }
     
-    
-        # 
-        # paste(values$genotype_nums[1], 
-        #   values$genotypes[1], "individuals,</br>",
-        #   values$genotype_nums[2], 
-        #   values$genotypes[2], "individuals, and </br>",
-        #   values$genotype_nums[3],
-        #   values$genotypes[3], "individuals")
   })
   
-  observeEvent(input$show_answer, {
-    show_ans(TRUE)
+  observeEvent(input$show_simple_answer, {
+    show_simple_ans(TRUE)
   })
   
   output$answer <- renderText({
-    if (show_ans()) {
-      sprintf("The frequency of allele %s is %0.3f. </br>
-               The frequency of allele %s is %0.3f. </br></br>
-               The frequency of genotype %s is %0.3f.</br>
-               The frequency of genotype %s is %0.3f.</br>
-               The frequency of genotype %s is %0.3f.</br></br>
-              The three genotype frequencies sum to %1.3f", 
-              # allele frequency sentences
-              values$a1, values$allele1,
-              values$a2, values$allele2,
-              # genotype sentences
-              values$genotypes[1], values$AA_freq,
-              values$genotypes[2], values$Aa_freq,
-              values$genotypes[3], values$aa_freq,
-              sum(values$AA_freq, values$Aa_freq, values$aa_freq))
-    }
-  })
-  output$check <- renderUI({
-    if (show_ans()) {
-      withMathJax(
-        HTML(sprintf("<hr><b>Check:</b></br>\\(\\sqrt{%0.3f}  = %0.3f ≠ %0.3f \\)</br>
-                \\(\\sqrt{%0.3f}  = %0.3f ≠ %0.3f\\)</br>",
-                     values$geno_freqs[1], 
-                     sqrt(values$geno_freqs[1]), 
-                     values$allele1_freq,
-                     values$geno_freqs[3], 
-                     sqrt(values$geno_freqs[3]), 
-                     values$allele2_freq))
+    if (show_simple_ans()) {
+      sprintf(
+        "The frequency of allele %s is %0.3f. </br>
+        The frequency of allele %s is %0.3f. </br></br>
+        The frequency of genotype %s is %0.3f.</br>
+        The frequency of genotype %s is %0.3f.</br>
+        The frequency of genotype %s is %0.3f.</br></br>
+        The three genotype frequencies sum to %1.3f",
+        # allele frequency sentences
+        simple$a1,
+        simple$allele1,
+        simple$a2,
+        simple$allele2,
+        # genotype sentences
+        simple$genotypes[1],
+        simple$AA_freq,
+        simple$genotypes[2],
+        simple$Aa_freq,
+        simple$genotypes[3],
+        simple$aa_freq,
+        sum(simple$AA_freq, simple$Aa_freq, simple$aa_freq)
       )
-      
     }
   })
 }
