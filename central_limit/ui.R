1d7ef40118991aad11aae36b2c8999802d487f5a
Michael S. Taylor (home) <mtaylor@semo.edu>
2019-11-23 19:02:11 -0600

    Reformat code.

--- central_limit/ui.R
+++ central_limit/ui.R
@@ -1,30 +1,38 @@
 library(ggplot2)
 library(shiny)
 
-ui <- navbarPage(theme = "semo_mods.css",
-                 windowTitle = "BI 163: Central Limit Theorem",
-                 title=div(img(src="semo_logo.png", height="70px"), 
-                           "Central Limit Theorem"),
+ui <- navbarPage(
+  theme = "semo_mods.css",
+  windowTitle = "BI 163: Central Limit Theorem",
+  title = div(
+    img(src = "semo_logo.png", height = "70px"),
+    "Central Limit Theorem"
+  ),
   tabPanel("Overview",
            mainPanel(
-             p("Introduction to Central Limit Theorem")
+             h2("Introduction to the Central Limit Theorem")
            )),
   tabPanel("Single Samples",
            sidebarLayout(
              sidebarPanel(
-               sliderInput("samp_size",
-                           "Sample Size",
-                           min = 10,
-                           max = 50,
-                           value = 30,
-                           step = 5),
+               sliderInput(
+                 "samp_size",
+                 "Sample Size",
+                 min = 10,
+                 max = 50,
+                 value = 10,
+                 step = 5
+               ),
                actionButton("sample_data",
                             "Sample Data"),
                actionButton("clear_data",
                             "Clear Means")
              ),
-           mainPanel(h4("Sample"),
-                     plotOutput("normal_plot"),
-                     tags$hr(),
-                     textOutput("sample_means"))
-)))
+             mainPanel(
+               h4("Sample"),
+               plotOutput("normal_plot"),
+               tags$hr(),
+               textOutput("sample_means")
+             )
+           ))
+)
