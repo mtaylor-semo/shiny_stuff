1fc016c36374de6c0be98ed65ba285c154f85b1f
Michael S. Taylor (home) <mtaylor@semo.edu>
2019-11-24 08:45:59 -0600

    Remove unnecessary lines, reformat code.

--- central_limit/server.R
+++ central_limit/server.R
@@ -5,7 +5,7 @@ library(shiny)
 # Global vars -------------------------------------------------------------
 
 pop_mean = 10 # Population mean
-pop_sd = 1.5
+pop_sd = 1.5  # Population std dev.
 
 sample_count_str <- "Sample"
 sample_mean_str <- "Mean:"
@@ -15,27 +15,30 @@ std_dev_str <- "Standard deviation:"
 # https://sebastiansauer.github.io/normal_curve_ggplot2/
 
 base_plot <- ggplot(data = data.frame(x = c(7, 13)), aes(x)) +
-#  stat_function(fun = dnorm, n = 101, args = list(mean = pop_mean, sd = pop_sd)) + 
   labs(x = "Mean",
        y = NULL)  +
   scale_y_continuous(breaks = NULL) +
   scale_x_continuous(breaks = 7:13) +
   theme_minimal() +
-  theme(title = element_text(size = 14),
-        axis.text = element_text(size = 14),
-        axis.title.x = element_text(size = 16)) +
+  theme(
+    title = element_text(size = 14),
+    axis.text = element_text(size = 14),
+    axis.title.x = element_text(size = 16)
+  ) +
   theme(panel.grid = element_blank(),
         axis.ticks = element_line(color = "gray"))
 
-curve_layer <- stat_function(fun = dnorm, n = 101, args = list(mean = pop_mean, sd = pop_sd))
+curve_layer <-
+  stat_function(fun = dnorm,
+                n = 101,
+                args = list(mean = pop_mean, sd = pop_sd))
 
 p1 <- base_plot + curve_layer
 
 samp_num <- 0
 grand_mean <- 0
 
-server <- function(input, output, session){
-
+server <- function(input, output, session) {
   output$normal_plot <- renderPlot(p1)
   
   output$many_plot <- renderPlot(base_plot)
@@ -44,7 +47,6 @@ server <- function(input, output, session){
   output$sample_mean <- renderText(sample_mean_str)
   output$standard_deviation <- renderText(std_dev_str)
   
-  
   reset_count <- eventReactive(input$clear_data, {
     output$sample_count <- renderText(sample_count_str)
     output$sample_mean <- renderText(sample_mean_str)
@@ -52,7 +54,7 @@ server <- function(input, output, session){
     output$sample_means <- renderText("")
   })
   
-# Single Sample -----------------------------------------------------------
+  # Single Sample -----------------------------------------------------------
   
   observeEvent(input$clear_data, {
     output$normal_plot <- renderPlot(p1)
@@ -62,60 +64,75 @@ server <- function(input, output, session){
     reset_count()
   })
   
-
-observeEvent(input$sample_data, {
-  samp <- rnorm(input$samp_size, mean = pop_mean, sd = pop_sd)
-  samp_mean <- mean(samp)
-  samp_sd <- round(sd(samp), 2)
-
-  samp_num <<- samp_num + 1 
-  grand_mean <<- grand_mean + samp_mean
-
-#    plot_title <- sprintf("Number: %i.\nMean: %.2f.\nStd Dev: %.2f.", 
-#                          samp_num, samp_mean, samp_sd)
   
-  p1 <<- p1 +
-    geom_vline(aes(xintercept = samp_mean),
-               color = "grey50",
-               size = 0.5) 
+  observeEvent(input$sample_data, {
+    samp <- rnorm(input$samp_size, mean = pop_mean, sd = pop_sd)
+    samp_mean <- mean(samp)
+    samp_sd <- round(sd(samp), 2)
+    
+    samp_num <<- samp_num + 1
+    grand_mean <<- grand_mean + samp_mean
+    
+    p1 <<- p1 +
+      geom_vline(aes(xintercept = samp_mean),
+                 color = "grey50",
+                 size = 0.5)
+    
+    p1 <- p1 +
+      geom_vline(aes(xintercept = samp_mean),
+                 color = "#9D2235",
+                 size = 1.1)
+    
+    output$normal_plot <- renderPlot(p1)
+    
+    output$sample_count <-
+      renderText(paste(sample_count_str, sprintf("%i", samp_num)))
+    output$sample_mean <-
+      renderText(paste(sample_mean_str, sprintf("%.2f", samp_mean)))
+    output$standard_deviation <-
+      renderText(paste(std_dev_str, sprintf("%.2f", samp_sd)))
+    
+    output$sample_means <-
+      renderText(sprintf(
+        "Mean of %i sample means: %.2f",
+        samp_num,
+        grand_mean / samp_num
+      ))
+  })
   
-  p1 <- p1 +
-    geom_vline(aes(xintercept = samp_mean), 
-               color = "#9D2235",
-               size = 1.1)# +
-    #labs(title = plot_title)
-
-  output$normal_plot <- renderPlot(p1)
   
-  output$sample_count <- renderText(paste(sample_count_str, sprintf("%i", samp_num)))
-  output$sample_mean <- renderText(paste(sample_mean_str, sprintf("%.2f", samp_mean)))
-  output$standard_deviation <- renderText(paste(std_dev_str, sprintf("%.2f", samp_sd)))
+  # Many samples ------------------------------------------------------------
   
-  output$sample_means <- renderText(sprintf("Mean of %i sample means: %.2f", samp_num, grand_mean/samp_num))
-})
-
-
-# Many samples ------------------------------------------------------------
-
-bin_width = 0.2
-
-observeEvent(input$sample_population,{
-  samples <- matrix(rnorm(input$number_samples*input$size_sample, 
-                          mean = pop_mean, 
-                          sd = pop_sd))  
-  sample_means <- data.frame(means = apply(samples, 1, mean))
+  bin_width = 0.2
   
-  output$many_plot <- renderPlot(
-     base_plot +
-       geom_histogram(data = sample_means,
-                      aes(x = means),
-                      color = "grey",
-                      binwidth = bin_width) +
-      stat_function(fun = function(x) dnorm(x, mean = pop_mean, sd = sd(samples)) * input$size_sample*input$number_samples * bin_width)
-  )
-
-  output$mean_of_means <- renderText(sprintf("Mean of %i sample means: %.2f", input$number_samples, mean(samples)))
-})
-
-
+  observeEvent(input$sample_population, {
+    samples <- matrix(rnorm(
+      input$number_samples * input$size_sample,
+      mean = pop_mean,
+      sd = pop_sd
+    ))
+    sample_means <- data.frame(means = apply(samples, 1, mean))
+    
+    output$many_plot <- renderPlot(
+      base_plot +
+        geom_histogram(
+          data = sample_means,
+          aes(x = means),
+          color = "grey",
+          binwidth = bin_width
+        ) +
+        stat_function(
+          fun = function(x)
+            dnorm(x, mean = pop_mean, sd = sd(samples)) * input$size_sample * input$number_samples * bin_width
+        )
+    )
+    
+    output$mean_of_means <-
+      renderText(sprintf(
+        "Mean of %i sample means: %.2f",
+        input$number_samples,
+        mean(samples)
+      ))
+  })
+  
 } # End server()
