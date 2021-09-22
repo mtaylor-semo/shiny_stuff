a5a753195ec59b1ee2ef19ab8029d7102302b675
Mike Taylor <mtaylor@semo.edu>
2021-09-22 17:51:27 -0500

    Working histograms.

--- biogeo/range_size/app.R
+++ biogeo/range_size/app.R
@@ -50,15 +50,15 @@ ui <- fluidPage(
                         unique(state_taxa$states)),
             radioButtons("taxon", 
                          "Choose a taxon", 
-                         choices = c("Crayfishes", "Fishes", "Mussels"), 
-                         selected = character(0))
+                         choices = c("Crayfishes", "Fishes", "Mussels"))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            #dataTableOutput("state_taxa_table"),
 #           verbatimTextOutput("state_taxa_table"),
-           verbatimTextOutput("the_taxon")
+           #verbatimTextOutput("the_taxon")
+            plotOutput("the_state")
         )
     )
 )
@@ -75,24 +75,42 @@ server <- function(input, output) {
         choices <- unique(state()$taxa)
         freezeReactiveValue(input, "taxon")
         updateRadioButtons(inputId = "taxon", 
-                           choices = choices, 
-                           selected = character(0))
+                           choices = choices)
     })
     
-    the_taxon <- reactive({
-        req(input$state)
-        filter(state_taxa, taxa == input$taxon)
-    })
-    
-    
-    output$the_taxon <- renderPrint({
-        freezeReactiveValue(input, "the_taxon")
-        the_file <- state_taxa %>% 
-            filter(states == input$state() &&
-                       taxa == input$taxon())
-        the_file
-   })
+    output$the_state <- renderPlot({
+        file_to_open <- paste0("data/",input$state, "_", input$taxon,".csv")
+        spp <- read.csv(file_to_open, row.names = 1)
 
+        numWatersheds <- colSums(spp)
+        numSpecies <- rowSums(spp)
+        bins <- seq(min(numWatersheds), max(numWatersheds))#, length.out = input$bins + 1)
+     
+        #paste(numWatersheds)   
+        nws <- nrow(spp)
+    #    highSp <- ceiling(max(numSpecies)/10)*10
+        
+        # draw the histogram with the specified number of bins
+        hist(numWatersheds, breaks=seq(0,nws,1), xlim = c(0,nws), xlab = 'Number of Watersheds', ylab = 'Number of Species', las=1, col = 'darkgray', border = 'white')
+    })
+    
+   #  the_taxon <- reactive({
+   #      req(input$state)
+   #      filter(state(), taxa == input$taxon)
+   #  })
+   #  
+   #  
+   #  observeEvent(the_taxon(), {
+   #      req(input$taxon)
+   #  })
+   #  
+   #  output$the_taxon <- renderPrint({
+   #      freezeReactiveValue(input, "the_taxon")
+   #      state_taxa %>% 
+   #          filter(states == input$state &
+   #                     taxa == input$taxon)
+   # })
+   # 
     
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
