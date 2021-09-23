f110cf0049db63efb9dc9753451481e9ec722536
Michael S. Taylor (home) <mtaylor@semo.edu>
2021-09-23 15:54:53 -0500

    Use ggplot2 for histogram.

--- biogeo/range_size/app.R
+++ biogeo/range_size/app.R
@@ -11,7 +11,8 @@ library(shiny)
 library(readr)
 library(dplyr)
 library(stringr)
-#library(magrittr)
+library(ggplot2)
+library(ggthemes)
 
 file_list <- list.files("data/")
 file_list_no_ext <- tools::file_path_sans_ext(file_list)
@@ -27,7 +28,7 @@ taxa <-
              sep = "_") %>% 
     str_to_title()
 
-state_taxa <- tibble(states, taxa, file_list)
+state_taxa <- tibble(states, taxa)
 
     
 # Define UI for application that draws a histogram
@@ -39,13 +40,16 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            selectInput("state",
-                        "Choose a state", 
-                        unique(state_taxa$states)),
-            radioButtons("taxon", 
-                         "Choose a taxon", 
-                         choices = c("Crayfishes", "Fishes", "Mussels"))
-        ),
+            selectInput(inputId = "state",
+                        label = "Choose a state", 
+                        choices = unique(states),
+                        selected = "Georgia",
+                        multiple = FALSE),
+            # radioButtons("taxon", 
+            #              "Choose a taxon", 
+            #              choices = c("Crayfishes", "Fishes", "Mussels"),
+            #              selected = "Fishes"),
+            uiOutput("dynamic_radio_buttons")),
 
         # Show the histogram
         mainPanel(
@@ -62,44 +66,48 @@ server <- function(input, output) {
                states == input$state)
     })
     
-    observeEvent(state(), {
+    # observeEvent(state(), {
+    #     choices <- unique(state()$taxa)
+    #     freezeReactiveValue(input, "taxon")
+    #     updateRadioButtons(inputId = "taxon", 
+    #                        choices = choices)
+    # })
+    
+    output$dynamic_radio_buttons <- renderUI({
         choices <- unique(state()$taxa)
-        freezeReactiveValue(input, "taxon")
-        updateRadioButtons(inputId = "taxon", 
-                           choices = choices)
+        freezeReactiveValue(input, "taxon1")
+        radioButtons(inputId = "taxon1", 
+                     "Choose a taxon", 
+                     choices = choices) #c("Crayfishes", "Fishes", "Mussels"))
     })
     
     output$the_histogram <- renderPlot({
-        file_to_open <- paste0("data/",input$state, "_", input$taxon,".csv")
+        file_to_open <- paste0("data/",input$state, "_", input$taxon1,".csv")
         spp <- read.csv(file_to_open, row.names = 1)
 
         numWatersheds <- colSums(spp)
         numSpecies <- rowSums(spp)
         bins <- seq(min(numWatersheds), max(numWatersheds))#, length.out = input$bins + 1)
+        dat <- tibble(numWatersheds)
      
         #paste(numWatersheds)   
         nws <- nrow(spp)
     #    highSp <- ceiling(max(numSpecies)/10)*10
         
         # draw the histogram with the specified number of bins
-        hist(numWatersheds, breaks=seq(0,nws,1), xlim = c(0,nws), xlab = 'Number of Watersheds', ylab = 'Number of Species', las=1, col = 'darkgray', border = 'white')
+        #hist(numWatersheds, breaks=seq(0,nws,1), xlim = c(0,nws), xlab = 'Number of Watersheds', ylab = 'Number of Species', las=1, col = 'darkgray', border = 'white')
+        ggplot(dat, aes(x=numWatersheds)) + 
+            geom_histogram(binwidth = 1,
+                           closed = "right", 
+                           breaks=seq(0,nws,1),
+                           color = "white") +
+            xlab("Number of Watersheds") +
+            ylab("Number of Species") +
+            xlim(0,nws) +
+            theme_minimal() 
     })
     
-
-    
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        numWatersheds <- colSums(spp)
-        numSpecies <- rowSums(spp)
-        bins <- seq(min(numWatersheds), max(numWatersheds), length.out = input$bins + 1)
-
-        nws <- nrow(spp)
-        highSp <- ceiling(max(numSpecies)/10)*10
-        
-        # draw the histogram with the specified number of bins
-        hist(numWatersheds, breaks=seq(0,nws,1), xlim = c(0,nws), xlab = 'Number of Watersheds', ylab = 'Number of Species', las=1, col = 'darkgray', border = 'white')
-    })
-}
+ }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
