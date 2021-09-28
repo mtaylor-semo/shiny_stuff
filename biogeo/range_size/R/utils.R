2ab95468fc5b0fb4213e7ccf86090054dc3a3d46
Michael S. Taylor (home) <mtaylor@semo.edu>
2021-09-28 04:58:44 -0500

    Generalized the open file functions to single function.

--- biogeo/range_size/R/utils.R
+++ biogeo/range_size/R/utils.R
@@ -1,19 +1,29 @@
 # Global functions --------------------------------------------------------
-# Global functions for the geographic range shiny app.
+# for the geographic range shiny app.
 
-# Open the data set. Can probably generalize these down the road
-# to open csv and tsv files, and also single function to open
-# all files.
-open_state_file <- function(st, tx) {
-  # State and taxon
-  the_state <- str_replace_all(st, " ", "_") # For two word states
-  file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
+# Open the data set. Can probably generalize these 
+# to open csv and tsv files.
+
+open_file <- function(tx, st = NULL) {
+  if (is.null(st)) {
+    file_to_open <- paste0("na_data/na_", tx, ".csv")
+  } else {
+    the_state <- str_replace_all(st, " ", "_") 
+    file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
+  }
   read.csv(file_to_open, row.names = 1)
 }
 
-open_na_file <- function(natx) {
-  # North American taxon
-  file_to_open <- paste0("na_data/na_", natx, ".csv")
-  read.csv(file_to_open, row.names = 1)
-}
-
+# open_state_file <- function(st, tx) {
+#   # State and taxon
+#   the_state <- str_replace_all(st, " ", "_") # For two word states
+#   file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
+#   read.csv(file_to_open, row.names = 1)
+# }
+# 
+# open_na_file <- function(natx) {
+#   # North American taxon
+#   file_to_open <- paste0("na_data/na_", natx, ".csv")
+#   read.csv(file_to_open, row.names = 1)
+# }
+# 
