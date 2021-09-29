750a92eb24b6f09c2af8d1b630ad1f50f3b548f5
Mike Taylor <mtaylor@semo.edu>
2021-09-29 14:33:27 -0500

    CA range extent plot now using ggplot2.

--- biogeo/range_size/app.R
+++ biogeo/range_size/app.R
@@ -359,6 +359,7 @@ server <- function(input, output, session) {
 
     } else { # plot 2.  Need better checks for the if/else
       
+      ## Convert much of this manipulation to dplyr / tidyverse
       mycolors <- c("#9d2235", "#003b5c")
       numRows <- nrow(cafish) ## number of species
       numCols <- ncol(cafish) ## Number of 1° latitude cells
@@ -395,17 +396,29 @@ server <- function(input, output, session) {
       } 
       
       cafish$latCol <- latCol
+      cafish$xrow <- seq(1:516)
       
       cafish <- cafish[order(-cafish$minLat,-cafish$meanLat),]
       
-      plot(nrow(cafish),99, type='n', xlim=c(1,516), ylim=c(-30,68), ylab='Latitude (°S — °N)', xlab='Species Index', main='Latitudinal Range for\nCalifornia Coastal Marine Fishes')
-      for (i in 1:numRows){
-        segments(x0 = i, y0 = cafish$minLat[i], x1 = i, y1 <- cafish$maxLat[i], col=cafish$latCol[i])
-        
-      }
-      abline(h=36,col='gray')
-      abline(h=meanCut,col='gray3')
-      abline(h=32,col='gray')
+      ggplot(cafish) +
+        geom_segment(aes(x = xrow, y = minLat, xend = xrow, yend = maxLat),
+                     color = latCol, size = 1.2) +
+        theme_minimal() +
+        ylab("Latitude (°S — °N)") +
+        xlab(NULL) +
+        geom_hline(yintercept = c(36, 32), col='gray') +
+        geom_hline(yintercept = meanCut, col = "gray10") +
+        scale_y_continuous(breaks = seq(-40, 70, 10)) +
+        theme(axis.text.x = element_blank())
+      
+      # plot(nrow(cafish),99, type='n', xlim=c(1,516), ylim=c(-30,68), ylab='Latitude (°S — °N)', xlab='Species Index', main='Latitudinal Range for\nCalifornia Coastal Marine Fishes')
+      # for (i in 1:numRows){
+      #   segments(x0 = i, y0 = cafish$minLat[i], x1 = i, y1 <- cafish$maxLat[i], col=cafish$latCol[i])
+      #   
+      # }
+      # abline(h=36,col='gray')
+      # abline(h=meanCut,col='gray3')
+      # abline(h=32,col='gray')
     }
   })
 
