2dc597da0da81d7874b603d73f340b5e06c36e16
Michael S. Taylor (home) <mtaylor@semo.edu>
2021-09-30 04:15:12 -0500

    Run through styler.

--- biogeo/range_size/R/utils.R
+++ biogeo/range_size/R/utils.R
@@ -1,7 +1,7 @@
 # Global functions --------------------------------------------------------
 # for the geographic range shiny app.
 
-# Open the data set. Can probably generalize these 
+# Open the data set. Can probably generalize these
 # to open csv and tsv files.
 
 open_file <- function(tx, st = NULL) {
@@ -9,14 +9,14 @@ open_file <- function(tx, st = NULL) {
     file_to_open <- paste0("na_data/na_", tx, ".csv")
   } else {
     switch(st,
-           "California" = {
-             file_to_open <- "marine/california_marine_fishes.csv"
-           },
-           {
-             the_state <- str_replace_all(st, " ", "_") 
-             file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
-           }
-           )
+      "California" = {
+        file_to_open <- "marine/california_marine_fishes.csv"
+      },
+      {
+        the_state <- str_replace_all(st, " ", "_")
+        file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
+      }
+    )
   }
   read.csv(file_to_open, row.names = 1)
 }
@@ -27,13 +27,13 @@ open_file <- function(tx, st = NULL) {
 #   file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
 #   read.csv(file_to_open, row.names = 1)
 # }
-# 
+#
 # open_na_file <- function(natx) {
 #   # North American taxon
 #   file_to_open <- paste0("na_data/na_", natx, ".csv")
 #   read.csv(file_to_open, row.names = 1)
 # }
-# 
+#
 
 ## Prediction check. Move requirement check for predictions here.
 ## sn = student_name, ps = pred_state, pn = pred_na, pc = pred_ca
@@ -43,8 +43,8 @@ pred_check <- function(sn = NULL, ps = NULL, pn = NULL, pc = NULL) {
 
 
 
-plotHistogram <- function(dat = NULL, x = NULL, closed = "right", breaks = c(y,z), ...) {
- ggplot(data = dat, aes(x = x)) +
+plotHistogram <- function(dat = NULL, x = NULL, closed = "right", breaks = c(y, z), ...) {
+  ggplot(data = dat, aes(x = x)) +
     geom_histogram(
       #        binwidth = 5,
       closed = closed,
--- biogeo/range_size/app.R
+++ biogeo/range_size/app.R
@@ -9,7 +9,7 @@ library(shiny)
 library(dplyr)
 library(stringr)
 library(ggplot2)
-#library(shinyjs)
+# library(shinyjs)
 
 # Global variables --------------------------------------------------------
 
@@ -25,8 +25,9 @@ states <- file_list_no_ext %>%
 
 taxa <-
   word(file_list_no_ext,
-       start = -1,
-       sep = "_") %>%
+    start = -1,
+    sep = "_"
+  ) %>%
   str_to_title()
 
 state_taxa <- tibble(states, taxa)
@@ -34,8 +35,8 @@ state_taxa <- tibble(states, taxa)
 # # Predefined tabs. See https://stackoverflow.com/a/60229331/3832941
 # # I predefined the tabPanels here so that I didn't clutter the
 # # server code.
-# state_tab <- tabPanel("test_tab", 
-#                       value = "tab2_val", 
+# state_tab <- tabPanel("test_tab",
+#                       value = "tab2_val",
 #                       br(),
 #                       h4("this is tab2"))
 
@@ -44,198 +45,225 @@ state_taxa <- tibble(states, taxa)
 
 ui <- tagList(
   includeCSS("www/semo_mods.css"),
-             navbarPage(id = "tabs",
-#  theme = "semo_mods.css",
-  windowTitle = "Biogeograpy: Geographic Range Size",
-  title = div(img(src = "semo_logo.png", height = "70px"),
-              "Geographic range size"),
-# Instructions tab ------------------------------------------------------------
+  navbarPage(
+    id = "tabs",
+    #  theme = "semo_mods.css",
+    windowTitle = "Biogeograpy: Geographic Range Size",
+    title = div(
+      img(src = "semo_logo.png", height = "70px"),
+      "Geographic range size"
+    ),
+    # Instructions tab ------------------------------------------------------------
 
-  tabPanel(
-    "Instructions",
-    mainPanel(
-      p("This app allows you to explore range sizes for three 
+    tabPanel(
+      "Instructions",
+      mainPanel(
+        p("This app allows you to explore range sizes for three
         aquatic groups (crayfishes, fishes, and mussels) for
         several states and for North America (primarily U.S.)."),
-      p("Choose the State tab to begin. Choose the state and the
+        p("Choose the State tab to begin. Choose the state and the
         taxon that was assigned to you."),
-      p("NOTE TO MST: Rework the assignment to have students 
+        p("NOTE TO MST: Rework the assignment to have students
         explore latitudal gradient, compare taxa within state, etc...")
-    )
-  ),
+      )
+    ),
 
 
-# Predictions tab ---------------------------------------------------------
-tabPanel("Predictions",
-         fluidRow(
-           column(1),
-           column(
-             2,
-             textInput("student_name", 
-                       "Enter your name:", 
-                       placeholder = "First Last"),
-             hr(),
-             p(),
-             p("Enter your predictions at right, then press the 
+    # Predictions tab ---------------------------------------------------------
+    tabPanel(
+      "Predictions",
+      fluidRow(
+        column(1),
+        column(
+          2,
+          textInput("student_name",
+            "Enter your name:",
+            placeholder = "First Last"
+          ),
+          hr(),
+          p(),
+          p("Enter your predictions at right, then press the
                'Next' button.")
-           ),
-           column(
-             4,
-             p("What do you predict for the state level? Will 
-               most species have small, moderate, or large range 
-               sizes?"
-             ),
-             textAreaInput(
-               "predict_state",
-               "Enter your prediction in this space:",
-               rows = 4,
-               placeholder = "State prediction…"
-             ),
-             p(),
-             hr(),
-             p("What do you predict for North America? Will 
-               most species have small, moderate, or large 
+        ),
+        column(
+          4,
+          p("What do you predict for the state level? Will
+               most species have small, moderate, or large range
+               sizes?"),
+          textAreaInput(
+            "predict_state",
+            "Enter your prediction in this space:",
+            rows = 4,
+            placeholder = "State prediction…"
+          ),
+          p(),
+          hr(),
+          p("What do you predict for North America? Will
+               most species have small, moderate, or large
                range sizes?"),
-             textAreaInput(
-               "predict_na",
-               "Enter your prediction in this space:",
-               rows = 4,
-               placeholder = "North America prediction…"
-             )
-           ),
-           column(
-             4,
-             p("What do you predict for the California marine 
-               fishes? Will most species have small, moderate, 
+          textAreaInput(
+            "predict_na",
+            "Enter your prediction in this space:",
+            rows = 4,
+            placeholder = "North America prediction…"
+          )
+        ),
+        column(
+          4,
+          p("What do you predict for the California marine
+               fishes? Will most species have small, moderate,
                or large range sizes?"),
-             textAreaInput(
-               "predict_ca",
-               "Enter your prediction in this space:",
-               rows = 4,
-               placeholder = "California prediction…"),
-             p(),
-             hr(),
-             p(),
-             span(textOutput("prediction_error"), style="color:#9D2235"),
-             actionButton(inputId = "next_pred", label = "Next", width = "35%")
-           )
-         )),
+          textAreaInput(
+            "predict_ca",
+            "Enter your prediction in this space:",
+            rows = 4,
+            placeholder = "California prediction…"
+          ),
+          p(),
+          hr(),
+          p(),
+          span(textOutput("prediction_error"), style = "color:#9D2235"),
+          actionButton(inputId = "next_pred", label = "Next", width = "35%")
+        )
+      )
+    ),
 
 
 
-# State tab -----------------------------------------------------------
+    # State tab -----------------------------------------------------------
 
-tabPanel("State",
-         fluidRow(
-           column(1),
-           column(
-             2,
-             wellPanel(p("Choose your state and then taxon
+    tabPanel(
+      "State",
+      fluidRow(
+        column(1),
+        column(
+          2,
+          wellPanel(
+            p("Choose your state and then taxon
                  to see the histogram."),
-             selectInput(
-               inputId = "state",
-               label = "Choose a state",
-               choices = unique(states),
-               selected = "Georgia",
-               multiple = FALSE
-             ),
-             uiOutput("dynamic_radio_buttons"),
-             hr(),
-             downloadButton('downloadReport')
-           )),
-           column(5, plotOutput("state_histogram")),
-           column(2, 
-                  uiOutput("state_numbers"),
-                  hr(),
-                  numericInput("bins", "Change the number of bins", min = 1, max = 5, value = 1, step = 1))
-         )),
+            selectInput(
+              inputId = "state",
+              label = "Choose a state",
+              choices = unique(states),
+              selected = "Georgia",
+              multiple = FALSE
+            ),
+            uiOutput("dynamic_radio_buttons"),
+            hr(),
+            downloadButton("downloadReport")
+          )
+        ),
+        column(5, plotOutput("state_histogram")),
+        column(
+          2,
+          uiOutput("state_numbers"),
+          hr(),
+          numericInput("bins", "Change the number of bins", min = 1, max = 5, value = 1, step = 1)
+        )
+      )
+    ),
 
 
-# North America tab -------------------------------------------------------
+    # North America tab -------------------------------------------------------
 
-tabPanel("North America",
-  fluidRow(
-    column(1),
-    column(
-      2, 
-      wellPanel(p("Range size for North America."
-      ),
-      radioButtons("na_taxon", 
-                   label = "Choose taxon:",
-                   choices = c("Fishes", "Mussels"),
-                   selected = "Fishes")
-      )),
-    column(5, plotOutput("na_histogram")),
-    column(2, 
-           uiOutput("na_numbers"),
-           hr(),
-           numericInput("na_bins", 
-                        "Change the number of bins", 
-                        min = 1, 
-                        max = 10, 
-                        value = 5, 
-                        step = 1)
-  )
+    tabPanel(
+      "North America",
+      fluidRow(
+        column(1),
+        column(
+          2,
+          wellPanel(
+            p("Range size for North America."),
+            radioButtons("na_taxon",
+              label = "Choose taxon:",
+              choices = c("Fishes", "Mussels"),
+              selected = "Fishes"
+            )
+          )
+        ),
+        column(5, plotOutput("na_histogram")),
+        column(
+          2,
+          uiOutput("na_numbers"),
+          hr(),
+          numericInput("na_bins",
+            "Change the number of bins",
+            min = 1,
+            max = 10,
+            value = 5,
+            step = 1
+          )
+        )
+      )
+    ),
+
+
+    # California Marine Fishes ------------------------------------------------
+
+    tabPanel(
+      "California Marine Fishes",
+      fluidRow(
+        column(1),
+        column(
+          2,
+          wellPanel(radioButtons(
+            inputId = "ca_marine",
+            label = "Choose plot type",
+            choices = c("Range size", "Range extent")
+          ))
+        ),
+        column(5, plotOutput("ca_marine_plot")),
+        column(
+          4,
+          p("This data set has 516 species."),
+          img(src = "california.png", width = "320px")
+        )
+      )
     )
-  ),
-
-
-# California Marine Fishes ------------------------------------------------
-
-tabPanel("California Marine Fishes",
-  fluidRow(
-    column(1),
-    column(2,
-           wellPanel(radioButtons(inputId = "ca_marine",
-                        label = "Choose plot type",
-                        choices = c("Range size", "Range extent")
-           )
-           )),
-    column(5, plotOutput("ca_marine_plot")),
-    column(4,
-           p("This data set has 516 species."),
-           img(src = "california.png", width = "320px"))
   )
-)
-)) # end UI
+) # end UI
 
 
 # Server ------------------------------------------------------------------
 
 server <- function(input, output, session) {
   session$onSessionEnded(stopApp)
-  
+
   output$prediction_error <- renderText({
-  if(input$student_name  == "" | 
-     input$predict_state == "" |
-     input$predict_na == "" |
-     input$predict_ca == "") {"Must fill in all blanks."}
+    if (input$student_name == "" |
+      input$predict_state == "" |
+      input$predict_na == "" |
+      input$predict_ca == "") {
+      "Must fill in all blanks."
+    }
   })
-  
+
   hideTab("tabs", "State")
   hideTab("tabs", "North America")
   hideTab("tabs", "California Marine Fishes")
-  
-## Reactive values ---------------------------------------------------------
-  
+
+  ## Reactive values ---------------------------------------------------------
+
   state <- reactive({
-    filter(state_taxa,
-           states == input$state)
+    filter(
+      state_taxa,
+      states == input$state
+    )
   })
 
   spp <- reactive({
-    #open_state_file(input$state, input$taxon)
+    # open_state_file(input$state, input$taxon)
     open_file(tx = input$taxon, st = input$state)
   })
-  
+
   spp_na <- reactive({
     open_file(tx = input$na_taxon)
   })
-  
-  plots <- reactiveValues(state = NULL, na = NULL, ca = NULL)
-  
 
-# Button observers --------------------------------------------------------
+  plots <- reactiveValues(state = NULL, na = NULL, ca = NULL)
+
+
+  # Button observers --------------------------------------------------------
 
   observeEvent(input$next_pred, {
     # Comment out for development.
@@ -260,9 +288,8 @@ server <- function(input, output, session) {
   # Report output idea from Shiny Gallery
   output$downloadReport <- downloadHandler(
     filename = function() {
-      paste('my-report', sep = '.', 'pdf')
+      paste("my-report", sep = ".", "pdf")
     },
-    
     content = function(file) {
       notification_id <- showNotification(
         "Generating report for download.",
@@ -270,79 +297,82 @@ server <- function(input, output, session) {
         closeButton = FALSE,
         type = "message"
       )
-      src <- normalizePath('range_report.Rmd')
-      
+      src <- normalizePath("range_report.Rmd")
+
       # temporarily switch to the temp dir, in case you do not have write
       # permission to the current working directory
       owd <- setwd(tempdir())
       on.exit(setwd(owd))
-      file.copy(src, 'range_report.Rmd', overwrite = TRUE)
+      file.copy(src, "range_report.Rmd", overwrite = TRUE)
 
-            
+
       library(rmarkdown)
-      out <- render('range_report.Rmd', 
-                    pdf_document(latex_engine = "lualatex"))
+      out <- render(
+        "range_report.Rmd",
+        pdf_document(latex_engine = "lualatex")
+      )
       file.rename(out, file)
       on.exit(removeNotification(notification_id), add = TRUE)
-      
     }
   )
 
-  
-## Outputs -------------------------------------------------------------
+
+  ## Outputs -------------------------------------------------------------
 
   output$dynamic_radio_buttons <- renderUI({
     choices <- unique(state()$taxa)
     freezeReactiveValue(input, "taxon")
-    radioButtons(inputId = "taxon",
-                 "Choose a taxon",
-                 choices = choices) #c("Crayfishes", "Fishes", "Mussels"))
+    radioButtons(
+      inputId = "taxon",
+      "Choose a taxon",
+      choices = choices
+    ) # c("Crayfishes", "Fishes", "Mussels"))
   })
-  
+
   output$state_numbers <- renderUI({
     dims <- dim(spp())
     sprintf("This data set has %d watersheds and %d species.", dims[1], dims[2])
   })
-  
+
   output$na_numbers <- renderUI({
     dims <- dim(spp_na())
     sprintf("This data set has %d watersheds and %d species.", dims[1], dims[2])
   })
-  
-  
-## State histograms ------------------------------------------------------
-  
+
+
+  ## State histograms ------------------------------------------------------
+
   output$state_histogram <- renderPlot({
     numWatersheds <- colSums(spp())
     numSpecies <- rowSums(spp())
-    
+
     nws <- nrow(spp())
-    
+
     bins <- input$bins
-    
+
     plots$state <- plotHistogram(dat = tibble(numWatersheds), x = numWatersheds, breaks = c(nws, input$bins))
-    
+
     plots$state
   })
-  
-## North America histogram -------------------------------------------------
-  
+
+  ## North America histogram -------------------------------------------------
+
   output$na_histogram <- renderPlot({
     numWatersheds <- colSums(spp_na())
     numSpecies <- rowSums(spp_na())
     bins <-
       seq(min(numWatersheds), max(numWatersheds))
     dat <- tibble(numWatersheds)
-    
+
     nws <- nrow(spp_na()) # Number of watersheds for x-axis
 
     plots$na <- plotHistogram(dat = tibble(numWatersheds), x = numWatersheds, breaks = c(nws, input$na_bins))
-    
+
     plots$na
   })
-  
 
-## California Marine plots -------------------------------------------------
+
+  ## California Marine plots -------------------------------------------------
 
   output$ca_marine_plot <- renderPlot({
     cafish <- open_file(st = "California")
@@ -350,82 +380,82 @@ server <- function(input, output, session) {
     if (input$ca_marine == "Range size") {
       rangeSize <- rowSums(cafish)
       numSpecies <- colSums(cafish)
-      
-      plots$ca <- plotHistogram(dat = tibble(rangeSize), x = rangeSize, breaks = c(100, 5))  + 
-        scale_x_continuous(breaks = seq(0,100,20)) +
-        xlab("Range size (degrees of latitude occupied)")
-    
-      plots$ca
 
+      plots$ca <- plotHistogram(dat = tibble(rangeSize), x = rangeSize, breaks = c(100, 5)) +
+        scale_x_continuous(breaks = seq(0, 100, 20)) +
+        xlab("Range size (degrees of latitude occupied)")
+
+      plots$ca
     } else { # plot 2.  Need better checks for the if/else
-      
+
       ## Convert much of this manipulation to dplyr / tidyverse
       mycolors <- c("#9d2235", "#003b5c")
       numRows <- nrow(cafish) ## number of species
       numCols <- ncol(cafish) ## Number of 1° latitude cells
-      
-      meanCut <- 34.4481  ## Point Conception latitude as cutoff for northern and southern species.
-      
-      meanLat <- rep(NA,numRows) ## Create a vector same length as number of species.
-      
-      minLat <- vector('numeric')
-      maxLat <- vector('numeric')
-      
+
+      meanCut <- 34.4481 ## Point Conception latitude as cutoff for northern and southern species.
+
+      meanLat <- rep(NA, numRows) ## Create a vector same length as number of species.
+
+      minLat <- vector("numeric")
+      maxLat <- vector("numeric")
+
       for (i in 1:numRows) {
-        x <- data.frame(cafish)[i,]
-        y <- colnames(x)[x==1]
-        
-        colNames <- gsub('N','',y)
-        colNames <- gsub('S','-',colNames)
-        
+        x <- data.frame(cafish)[i, ]
+        y <- colnames(x)[x == 1]
+
+        colNames <- gsub("N", "", y)
+        colNames <- gsub("S", "-", colNames)
+
         minLat[i] <- as.numeric(colNames[1])
         maxLat[i] <- as.numeric(colNames[length(colNames)])
         meanLat[i] <- mean(as.numeric(colNames))
       }
-      
+
       cafish$minLat <- minLat
       cafish$maxLat <- maxLat
       cafish$meanLat <- meanLat
 
       print(head(cafish))
-      
-      latCol <- vector('character')
+
+      latCol <- vector("character")
       for (i in 1:numRows) {
-        if (cafish$meanLat[i] > meanCut){latCol[i] = mycolors[1]}
-        else { latCol[i] = mycolors[2]}	
-      } 
-      
+        if (cafish$meanLat[i] > meanCut) {
+          latCol[i] <- mycolors[1]
+        } else {
+          latCol[i] <- mycolors[2]
+        }
+      }
+
       cafish$latCol <- latCol
       cafish$xrow <- seq(1:516)
-      
-      cafish <- cafish[order(-cafish$minLat,-cafish$meanLat),]
-      
+
+      cafish <- cafish[order(-cafish$minLat, -cafish$meanLat), ]
+
       ggplot(cafish) +
         geom_segment(aes(x = xrow, y = minLat, xend = xrow, yend = maxLat),
-                     color = latCol, size = 1.2) +
+          color = latCol, size = 1.2
+        ) +
         theme_minimal() +
         ylab("Latitude (°S — °N)") +
         xlab(NULL) +
-        geom_hline(yintercept = c(36, 32), col='gray') +
+        geom_hline(yintercept = c(36, 32), col = "gray") +
         geom_hline(yintercept = meanCut, col = "gray10") +
         scale_y_continuous(breaks = seq(-40, 70, 10)) +
         theme(axis.text.x = element_blank())
-      
+
       # plot(nrow(cafish),99, type='n', xlim=c(1,516), ylim=c(-30,68), ylab='Latitude (°S — °N)', xlab='Species Index', main='Latitudinal Range for\nCalifornia Coastal Marine Fishes')
       # for (i in 1:numRows){
       #   segments(x0 = i, y0 = cafish$minLat[i], x1 = i, y1 <- cafish$maxLat[i], col=cafish$latCol[i])
-      #   
+      #
       # }
       # abline(h=36,col='gray')
       # abline(h=meanCut,col='gray3')
       # abline(h=32,col='gray')
     }
   })
-
 }
 
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
-
